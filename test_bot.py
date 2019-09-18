3b57758f64f53906bc68b1810c7f2d5623ad6baf
alkhamil <alkhamilnaz@gmail.com>
2019-09-18 21:02:12 +0700

    telegram_bot

---
+++ test_bot.py
@@ -0,0 +1,18 @@
+
+import requests
+
+def telegram_bot_sendtext(bot_message):
+    
+    bot_token = 'APIKEY'
+    bot_chatID = [
+        {"users" : "nazmudin", "id_bot" : "329264245"},
+        {"users" : "raka", "id_bot" : "602980254"}
+    ]
+    
+    for bot_id in bot_chatID:
+        send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_id['id_bot'] + '&parse_mode=Markdown&text=' + bot_message
+        response = requests.get(send_text)
+    
+    return response.json()
+
+telegram_bot_sendtext("Test")
